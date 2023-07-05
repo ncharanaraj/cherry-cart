0a4d53dd0b58185c9f864221376f6d073f76bf62
Charanaraj N <goudacharanraj760@gmail.com>
2023-07-05 12:05:08 +0530

    P0 pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import ProductListing from "./components/productListing/ProductListing";
+import ProductDetails from "./components/productDetails/ProductDetails";
+import Cart from "./components/cart/Cart";
 
 function App() {
-  return <></>;
+  return (
+    <div className="app">
+      <ProductListing />
+      <ProductDetails />
+      <Cart />
+    </div>
+  );
 }
 
 export default App;
