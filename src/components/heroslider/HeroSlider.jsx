721ad008e59aab652d472ea9bb1b902a8c5fb9fb
Charanaraj N <goudacharanraj760@gmail.com>
2023-07-24 18:09:24 +0530

    style: heroslider

---
+++ src/components/heroslider/HeroSlider.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import "./heroSlider.css";
+import { Link } from "react-router-dom";
+
+const sliderImages = [
+  "https://m.media-amazon.com/images/G/31/img21/MA2023/PD23/sbcheader/Hero._SX3000_QL85_FMpng_.png?",
+  "https://m.media-amazon.com/images/G/31/img23/Fashion/Event/JuneWRS/eventpage/pc/CML/CML-1._SX3000_QL85_.jpg",
+  "https://m.media-amazon.com/images/G/31/img23/Fashion/Event/JuneWRS/eventpage/pc/CML/CML-8._SX3000_QL85_.jpg?",
+];
+
+const HeroSlider = () => {
+  const [selectedSlide, setSelectedSlide] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setSelectedSlide((selectedSlide) =>
+        selectedSlide < 2 ? selectedSlide + 1 : 0
+      );
+    }, 4000);
+  }, []);
+
+  return (
+    <div>
+      <Link to="/store">
+        <img src={sliderImages[selectedSlide]} alt="" srcset="" />
+      </Link>
+    </div>
+  );
+};
+
+export default HeroSlider;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import HeroSlider from "../../components/heroslider/HeroSlider";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <HeroSlider />
+    </div>
+  );
 };
 
 export default Home;
